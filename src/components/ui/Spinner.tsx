361ee3b3b70d88381dde2591e420922df7356e92
Rodrigo Frenk <furenku@gmail.com>
2025-04-24 21:35:05 -0600

    add spinner

---
+++ src/components/ui/Spinner.tsx
@@ -0,0 +1,7 @@
+export const Spinner = () => {
+    return (        
+        <div className="w-full h-full bg-gray-200 flex justify-center items-center">
+            <div className="animate-spin rounded-full border-4 border-x-transparent border-b-gray-400 w-16 h-16"></div>
+        </div>
+    );
+};
